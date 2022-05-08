25a0453f17f6bed71927a39a941af5ae3665c2e3
LinWang1225 <534605478@qq.com>
2022-05-09 01:02:51 +0800

    chore(gamescene):supplement of head file

--- Classes/View/GameScene.h
+++ Classes/View/GameScene.h
@@ -1,5 +1,4 @@
 #pragma once
-
 //#define NDEBUG
 #include<cassert>
 #include "cocos2d.h"
@@ -7,19 +6,49 @@ USING_NS_CC;
 #include"ui/CocosGUI.h"
 using namespace ui;
 #include"Tool/SceneManager.h"
+#include "Tool/MapInfo.h"
+#include"Hero/HeroSprite.h"
+#include"Hero/BeiyaHero.h"
 
 class GameScene : public Scene
 {
 public:
-
     CREATE_FUNC(GameScene);
-
-private:
 
     virtual bool init();
 
-private:
+    /********* @author:张靖凯************/
+    //传入主角的坐标，放在控制器（控制角色）当中
+    void setViewPointByPlayer(Point position);
 
+    //判断是否碰撞障碍物（有bug！）(暂时不用)
+    //传入的是openGL坐标(就是左下角为原点，3000*3000）向右为x向上为y
+    bool isColliding(Vec2 position);
+
+    //转换成瓦片坐标（左上角为原点）
+    Vec2 tiledCoordFromPosition(Vec2 position);
+
+    //初始化地图
+    void initMap();
+
+    /********* @author:王琳************/
+    
+    void update(float dt);
+
+private:
+    //屏幕大小
     const Vec2 kVisibleSize = Director::getInstance()->getVisibleSize();
 
+
+
+
+    /********* @author:张靖凯************/
+
+    //地图信息（initMap()函数初始化了里面的变量）
+    MapInfo _mapinfo;
+    //主角
+    Beiya* _player;
+   
 };
+
+
