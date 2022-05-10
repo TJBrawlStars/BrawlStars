a05b779e97b80a682dbf1203589f162e3fcf160a
Mikeneko-nya <mayaaowu@gmail.com>
2022-05-10 21:00:10 +0800

    feat($View)：更新room场景基本内容
    是一个层，贴在main场景上，支持机器人添加减少等

--- Classes/View/RoomLayer.cpp
+++ Classes/View/RoomLayer.cpp
@@ -1,31 +1,185 @@
 #include "RoomLayer.h"
+USING_NS_CC;
+using namespace ui;
+//#define NDEBUG
+#include<cassert>
+#include "Tool/SceneManager.h"
+#include"Tool/Tools.h"
 #pragma execution_character_set("utf-8")  
+
+static constexpr int kEnemyTag = 0;
+static constexpr int kOurTag = 1;
 
 bool RoomLayer::init()
 {
 	if (!Layer::init())
 		return false;
 
+	//设置吞噬点击事件
+	auto listener = EventListenerTouchOneByOne::create();
+	listener->onTouchBegan = CC_CALLBACK_2(RoomLayer::onTouchBegan, this);
+	Tools::LayerSwallow(listener, this);
+
 	//层背景
-	_bg = Sprite::create("HelloWorld.png");
+	_bg = Tools::SetBg(this);
+	/*_bg = Scale9Sprite::create("ui/button.png");
 	assert(_bg != NULL);
-	_bg->setPosition(kVisibleSize / 2);
 	this->addChild(_bg);
+	_bg->setCapInsets(Rect(5, 5, 15, 15));
+	_bg->setPosition(kVisibleSize / 2);
+	_bg->setContentSize(kVisibleSize);*/
 
 	//开始游戏按钮
-	_start = Button::create("CloseNormal.png");
-	assert(_start != NULL);
-	_start->setPosition(kVisibleSize / 2);
-	this->addChild(_start);
-	_start->addClickEventListener([](Ref*)
+	_start = Tools::ButtonCreate("开始对战", Vec2(kVisibleSize.width - 130, 75), this);
+	_start->addTouchEventListener([this](Ref*, Widget::TouchEventType type)
 		{
-			SceneManager::getInstance()->changeScene(SceneManager::EnumSceneType::en_GameScene);
+			if (type == Widget::TouchEventType::ENDED)
+				SceneManager::getInstance()->changeScene(SceneManager::EnumSceneType::en_GameScene);
 		});
 
-	auto label = Label::createWithSystemFont("房间层", "方正姚体", 50);
-	label->setPosition(kVisibleSize.x / 2, kVisibleSize.y / 2 - 200);
-	this->addChild(label);
+	//返回按钮
+	_back = Tools::ButtonCreate(Vec2(77, kVisibleSize.height - 40), "ui/back.png", this);
+	_back->addTouchEventListener([this](Ref*, Widget::TouchEventType type)
+		{
+			if (type == Widget::TouchEventType::ENDED)
+				Tools::SwitchScene(this, Tools::SwitchSceneType::Up);
+		});
 
+	//6个房间成员框
+	MemberCreate();
+
+	//VS字样
+	_vs = Tools::LabelCreateSystem(kVisibleSize / 2, "友谊战", "微软雅黑", 70, this);
+
+	//自动补全机器人按钮
+	_robots = Tools::ButtonCreate("补全机器人", Vec2(125, 100), this);
+	_robots->addTouchEventListener([this](Ref*, Widget::TouchEventType type)
+		{
+			//设置我方和敌方机器人个数
+			if (type == Widget::TouchEventType::ENDED)
+			{
+				/*while (_enemy.setRobot(true));
+				while (_our.setRobot(true));*/
+				while (_member.setRobot(true));
+				//遍历_figures实现翻转(使用基于范围的For循环)
+				for (auto item : _figures)
+				{
+					if (item->getSelectedIndex() == 1);
+					else if(item->getSelectedIndex() == 0)
+						item->setSelectedIndex(1);
+				}
+			}
+		});
 
 	return true;
 }
+
+bool RoomLayer::onTouchBegan(cocos2d::Touch*, cocos2d::Event*)
+{
+	return true;
+}
+
+//弃用
+
+//Button* RoomLayer::ButtonCreate(const cocos2d::Vec2&& position, const std::string&& pic_name)
+//{
+//	auto button = Button::create("ui/button.png");
+//	assert(button != NULL);
+//
+//	button->setScale9Enabled(true);
+//	button->setCapInsets(Rect(5, 5, 15, 15));
+//	button->setPosition(position);
+//
+//	auto sprite = Sprite::create(pic_name);
+//	assert(sprite != NULL);
+//
+//	button->addChild(sprite);
+//	button->setContentSize(Size(sprite->getContentSize().width + 50, sprite->getContentSize().height + 25));
+//	sprite->setPosition(button->getContentSize() / 2);
+//	this->addChild(button);
+//	return button;
+//}
+//
+//Button* RoomLayer::ButtonCreate(const std::string&& words, const cocos2d::Vec2&& position)
+//{
+//	auto button = Button::create("ui/button_y.png");
+//	assert(button != NULL);
+//
+//	button->setScale9Enabled(true);
+//	button->setCapInsets(Rect(5, 5, 15, 15));
+//	button->setPosition(position);
+//
+//	auto label = Label::createWithSystemFont(words, "微软雅黑", 30);
+//	assert(label != NULL);
+//	button->setTitleLabel(label);
+//	button->setContentSize(Size(label->getContentSize().width + 25, label->getContentSize().height + 25));
+//	this->addChild(button);
+//	return button;
+//}
+
+void RoomLayer::MemberCreate()
+{
+	//读取main场景的精灵
+	//先把我方房主设置上
+	PlusMemberCreate();
+}
+
+void RoomLayer::PlusMemberCreate()
+{
+	for (int i = 1; i <= 9; ++i)
+	{
+		auto box1 = MenuItemImage::create("ui/box.png", "ui/box.png");
+		auto box2 = MenuItemImage::create("ui/box_robot.png", "ui/box_robot.png");
+		assert(box1 != NULL);
+		assert(box2 != NULL);
+		//这边如果联机的话这个callback函数要改一下
+		auto box0 = MenuItemToggle::createWithCallback(CC_CALLBACK_1(RoomLayer::menuCloseCallback, this), box1, box2, NULL);
+		_figures.pushBack(box0);
+		auto box = Menu::create(box0, NULL);
+		this->addChild(box);
+
+		if (i <= 5)
+		{
+			//设置tag，方便之后根据tag控制哪个Menber成员的增减
+			//box0->setTag(kEnemyTag);
+			box->setPosition(Vec2(kVisibleSize.width / 2 + (i - 3) * 150, kVisibleSize.height / 2 + 200));
+		}
+		else
+		{
+			//设置tag，方便之后根据tag控制哪个Menber成员的增减
+			//box0->setTag(kOurTag);
+			box->setPosition(Vec2(kVisibleSize.width / 2 + (i - 7) * 150, kVisibleSize.height / 2 - 200));
+		}
+		box->setContentSize(Size(50, 125));
+	    
+	}
+}
+
+void RoomLayer::menuCloseCallback(Ref* pSender)
+{
+	//这边如果要有真人还需要来一个层选择，选择之后传给这里，然后来一个玩家的角色图像把原本机器人的图像盖住吧？（总之之后再说
+	//或许可以加一个选择按钮的第二次点击是添加真人玩家？
+	auto const item = dynamic_cast<MenuItemToggle*>(pSender);
+	if (item)
+	{
+		if (item->getSelectedIndex()==1)
+		{
+			/*if (item->getTag() == kOurTag)
+				_our.setRobot(true);
+			else if (item->getTag() == kEnemyTag)
+				_enemy.setRobot(true);*/
+			_member.setRobot(true);
+		}
+		else
+		{
+			/*if (item->getTag() == kOurTag)
+				_our.setRobot(false);
+			else if (item->getTag() == kEnemyTag)
+				_enemy.setRobot(false);*/
+			_member.setRobot(false);
+		}
+	}
+}
+
+
+		
--- Classes/View/RoomLayer.h
+++ Classes/View/RoomLayer.h
@@ -1,38 +1,88 @@
 #pragma once
 
-//#define NDEBUG
-#include<cassert>
 #include "cocos2d.h"
-USING_NS_CC;
 #include"ui/CocosGUI.h"
-using namespace ui;
-#include "Tool/SceneManager.h"
+#include"Tool/Member.h"
+
+class Member;
 
 /**
 * @brief 房间场景
 */
-class RoomLayer :public Layer
+class RoomLayer :public cocos2d::Layer
 {
 public:
 
 	CREATE_FUNC(RoomLayer);
 
+	/**
+    * @brief 成员的点击函数
+    */
+	void menuCloseCallback(Ref* pSender);
+
+
 private:
 
 	virtual bool init();
 
+	virtual bool onTouchBegan(cocos2d::Touch*, cocos2d::Event*);
+
+	RoomLayer() :kVisibleSize(cocos2d::Director::getInstance()->getVisibleSize())
+		, _bg(NULL), _choose_hero(NULL), _start(NULL), _back(NULL), _vs(NULL), _robots(NULL), _member() {};
+
+	//弃用
+
+	//**
+ //   * @brief 设置按钮并add进场景,有两个重载版本
+ //   * @param (Vec2 position)位置
+ //   * @param (std::string pic_name)图片路径
+	//* @param (std::string words)文字按钮的显示文字
+ //   */
+	//cocos2d::ui::Button* ButtonCreate(const cocos2d::Vec2&& position, const std::string&& pic_name);
+	//cocos2d::ui::Button* ButtonCreate(const std::string&& words, const cocos2d::Vec2&& position);
+
+	/**
+	* @brief 设置6个房间成员框，包括一个房主框和5个+框
+	*/
+	void MemberCreate();
+
+	/**
+	* @brief 5个+框
+	*/
+	void PlusMemberCreate();
+
 private:
 
-	const Vec2 kVisibleSize = Director::getInstance()->getVisibleSize();
+	const cocos2d::Size kVisibleSize;
 
 	//登录画面背景
-	Sprite* _bg = NULL;
+	cocos2d::ui::Scale9Sprite* _bg;
 
 	//选择hero按钮
-	Button* _choose_hero = NULL;
+	cocos2d::ui::Button* _choose_hero;
 
 	//开始对战按钮
-	Button* _start;
+	cocos2d::ui::Button* _start;
+	
+	// 返回按钮
+	cocos2d::ui::Button* _back;
+	
+	//vs标识
+	cocos2d::Label* _vs;
+
+	//自动补全机器人按钮
+	cocos2d::ui::Button* _robots;
+
+	////敌方人数
+	//Member _enemy;
+
+	////我方人数
+	//Member _our;
+
+	Member _member;
+
+	//成员
+	cocos2d::Vector<cocos2d::MenuItemToggle*> _figures;
 
 };
 
