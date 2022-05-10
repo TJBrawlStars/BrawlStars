6c4212f479ba375596dea553083c10d7bcbeb3c0
Mikeneko-nya <mayaaowu@gmail.com>
2022-05-10 21:05:50 +0800

    feat($View)：更新结算页面基本内容

--- Classes/View/SettlementScene.cpp
+++ Classes/View/SettlementScene.cpp
@@ -1,4 +1,10 @@
 #include "SettlementScene.h"
+USING_NS_CC;
+using namespace ui;
+//#define NDEBUG
+#include<cassert>
+#include "Tool/SceneManager.h"
+#include"Tool/Tools.h"
 #pragma execution_character_set("utf-8")  
 
 bool SettlementScene::init()
@@ -6,19 +12,41 @@ bool SettlementScene::init()
 	if (!Scene::init())
 		return false;
 
-	_continue = Button::create("CloseNormal.png");
-	assert(_continue);
-	_continue->setPosition(kVisibleSize / 2);
-	this->addChild(_continue);
+	//背景
+	_bg = Tools::SetBg("ui/settlementBg.png", this);
+
+    //继续按钮
+	_continue = Tools::ButtonCreate("继 续", Vec2(kVisibleSize.width / 2, 70), this);
 	_continue->addClickEventListener([](Ref*)
 		{
 			SceneManager::getInstance()->changeScene(SceneManager::EnumSceneType::en_MainScene);
 		});
 
-	auto label = Label::createWithSystemFont("结算界面", "方正姚体", 50);
-	assert(label);
-	label->setPosition(kVisibleSize.x / 2, kVisibleSize.y / 2 - 100);
-	this->addChild(label);
+    //从游戏控制器中读出我方和敌方的hero然后把照片贴上来,然后对应的战绩也要有,在底下+标签叭
+
+	//从游戏控制器中读出胜利方，然后在指定一边显示win
+	WhoWin();
 
 	return true;
+}
+
+void SettlementScene::WhoWin()
+{
+	//读游戏控制器的操作，这边就先直接我方win叭
+	auto win = true;
+
+	auto label = Label::createWithTTF("W I N", "fonts/arial.ttf", 75);
+	assert(label != 0);
+	this->addChild(label);
+
+	if (win)
+		label->setPosition(Vec2(kVisibleSize.width / 2 - 450, kVisibleSize.height - 100));
+	else
+		label->setPosition(Vec2(kVisibleSize.width / 2 + 450, kVisibleSize.height - 100));
+}
+
+void SettlementScene::ShowRecord()
+{
+	//用for的那个用法批量创建图片
+	//同上，批量创建label，都写在TOOl里叭，那个类名字改一下
 }
--- Classes/View/SettlementScene.h
+++ Classes/View/SettlementScene.h
@@ -1,17 +1,12 @@
 #pragma once
 
-//#define NDEBUG
-#include<cassert>
 #include "cocos2d.h"
-USING_NS_CC;
 #include"ui/CocosGUI.h"
-using namespace ui;
-#include "Tool/SceneManager.h"
 
 /**
  * @brief 结算场景
  */
-class SettlementScene : public Scene
+class SettlementScene : public cocos2d::Scene
 {
 public:
 
@@ -21,11 +16,27 @@ private:
 
     virtual bool init();
 
+    SettlementScene() :kVisibleSize(cocos2d::Director::getInstance()->getVisibleSize())
+        , _continue(NULL), _bg(NULL) {};
+
+    /**
+    * @brief 从游戏控制器中读出胜利方，然后在指定一边显示win
+    */
+    void WhoWin();
+
+    /**
+    * @brief 从游戏控制器中读出我方和敌方的hero然后把照片贴上来,然后对应的战绩也要有,在底下+标签叭
+    */
+    void ShowRecord();
+
 private:
      
-    const Vec2 kVisibleSize = Director::getInstance()->getVisibleSize();
+    const cocos2d::Size kVisibleSize;
     
     //继续按钮
-    Button* _continue = NULL;
+    cocos2d::ui::Button* _continue;
+
+    //背景
+    cocos2d::Sprite* _bg;
 
 };
