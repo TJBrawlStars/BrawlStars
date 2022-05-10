bcadd8829c065226e0568664e261208c146ff4ac
Mikeneko-nya <mayaaowu@gmail.com>
2022-05-10 21:04:45 +0800

    feat($View)：更新main场景基本功能，结合setting、loading、room层

--- Classes/View/MainScene.cpp
+++ Classes/View/MainScene.cpp
@@ -1,4 +1,14 @@
 #include "MainScene.h"
+USING_NS_CC;
+using namespace ui;
+//#define NDEBUG
+#include<cassert>
+#include "LoadingScene.h"
+#include "RoomLayer.h"
+#include "SettingLayer.h"
+#include "Tool/Setting.h"
+#include "Tool/SceneManager.h"
+#include "Tool/Tools.h"
 #pragma execution_character_set("utf-8")  
 
 bool MainScene::init()
@@ -6,40 +16,66 @@ bool MainScene::init()
 	if (!Scene::init())
 		return false;
 
-	////设置背景
-	//_bg = Sprite::create("");
-	//assert(_bg != NULL);
-	//this->addChild(_bg);
-	//_bg->setPosition(kVisibleSize / 2);
-
 	//设置loading界面
 	if (SceneManager::getInstance()->isFirst())
 	{
+		//初始化Tools
+		Tools::set();
+
 		_loading = LoadingScene::create();
 		assert(_loading != NULL);
 		this->addChild(_loading, 100);
 	}
 
-	////各种按钮的实现
-	//_menu_button = ButtonCreate();
-	//_menu_button->addClickEventListener([](Ref*)
-	//	{
-	//		//这里塞一个层，层上有各种按钮
-	//	});
-	//_info_button= ButtonCreate();
-	//_info_button->addClickEventListener([](Ref*)
-	//	{
-	//		//弹出个人信息显示层
-	//	});
-	_play_button = Button::create("CloseNormal.png");
-	_play_button->setPosition(kVisibleSize / 2);
-	this->addChild(_play_button);
-	_play_button->addClickEventListener([this](Ref*)
+	//设置背景
+	_bg = Tools::SetBg("ui/MainBg.png", this);
+
+	//各种按钮的实现
+	_menu_button = Tools::ButtonCreate(Vec2(275, kVisibleSize.height - 40), "ui/menu.png", this);
+	_menu_button->addTouchEventListener([this](Ref*, Widget::TouchEventType type)
 		{
-			_room = RoomLayer::create();
-			_room->setPosition(Vec2(0,0));
-			assert(_room);
-			this->addChild(_room);
+			if (type == Widget::TouchEventType::ENDED)
+			{
+				_set = SettingLayer::create();
+				assert(_set);
+				this->addChild(_set);
+				Tools::SwitchScene(_set, Tools::SwitchSceneType::Down);
+			}
+		});
+
+	_info_button = Tools::ButtonCreate(Vec2(77, kVisibleSize.height - 40), "ui/info.png", this);
+	_info_button->addTouchEventListener([this](Ref*, Widget::TouchEventType type)
+		{
+			if (type == Widget::TouchEventType::ENDED)
+			{
+				//弹出个人信息显示层
+			}
+		});
+
+	_play_button = Tools::ButtonCreate("对   战", Vec2(kVisibleSize.width - 200, 75), this);
+	_play_button->addTouchEventListener([this](Ref*, Widget::TouchEventType type)
+		{
+			if (type == Widget::TouchEventType::ENDED)
+			{
+				_room = RoomLayer::create();
+				assert(_room);
+				this->addChild(_room);
+				Tools::SwitchScene(_room, Tools::SwitchSceneType::Down);
+			}
+		});
+
+	_back = Tools::ButtonCreate(Vec2(175, kVisibleSize.height - 40), "ui/back.png", this);
+	_back->addTouchEventListener([this](Ref*, Widget::TouchEventType type)
+		{
+			//不这样的话点击和结束点击都会创建一个层，坑死我了
+			if (type == Widget::TouchEventType::ENDED)
+			{
+				Setting::getInstance()->StopSound();
+				_loading = LoadingScene::create();
+				assert(_loading != NULL);
+				this->addChild(_loading, 100);
+				Tools::SwitchScene(_loading, Tools::SwitchSceneType::FadeIn);
+			}
 		});
 
 	////主界面显示的人物
@@ -48,27 +84,45 @@ bool MainScene::init()
 	//_figure->setPosition(kVisibleSize / 2);
 	//this->addChild(_figure);
 
-	auto label = Label::createWithSystemFont("主界面", "方正姚体", 50);
-	label->setPosition(kVisibleSize.x / 2, kVisibleSize.y / 2 - 100);
-	this->addChild(label);
-
-
 	return true;
 }
 
-Button* MainScene::ButtonCreate(Vec2 position, std::string pic_name1, std::string pic_name2)
-{
-	assert(position.x<0 || position.x>kVisibleSize.x || position.y<0 || position.y>kVisibleSize.y);
+//弃用
 
-	decltype(Button::create()) button;
-	if (pic_name2 == "")
-		button = Button::create(pic_name1);
-	else
-		button = Button::create(pic_name1, pic_name2);
+//Button* MainScene::ButtonCreate(const Vec2&& position, const std::string&& pic_name)
+//{
+//	auto button = Button::create("ui/button.png");
+//	assert(button != NULL);
+//
+//	button->setScale9Enabled(true);
+//	button->setCapInsets(Rect(5, 5, 15, 15));
+//	button->setPosition(position);
+//
+//	auto sprite = Sprite::create(pic_name);
+//	assert(sprite != NULL);
+//
+//	button->addChild(sprite);
+//	button->setContentSize(Size(sprite->getContentSize().width + 50, sprite->getContentSize().height + 25));
+//	sprite->setPosition(button->getContentSize() / 2);
+//	this->addChild(button);
+//	return button;
+//}
 
-	assert(button != NULL);
+//弃用
 
-	button->setPosition(position);
-	this->addChild(button);
-	return button;
-}
+//Button* MainScene::ButtonCreate(const std::string&& words, const Vec2&& position)
+//{
+//	auto button = Button::create("ui/button_y.png");
+//	assert(button != NULL);
+//
+//	button->setScale9Enabled(true);
+//	button->setCapInsets(Rect(5, 5, 15, 15));
+//	button->setPosition(position);
+//
+//	auto label = Label::createWithSystemFont(words, "微软雅黑", 50);
+//	assert(label != NULL);
+//	button->setTitleLabel(label);
+//	button->setContentSize(Size(label->getContentSize().width + 125, label->getContentSize().height + 50));
+//	this->addChild(button);
+//	return button;
+//}
--- Classes/View/MainScene.h
+++ Classes/View/MainScene.h
@@ -1,21 +1,16 @@
 #pragma once
 
-//#define NDEBUG
-#include<cassert>
 #include "cocos2d.h"
-USING_NS_CC;
 #include"ui/CocosGUI.h"
-using namespace ui;
-#include "LoadingScene.h"
-#include "RoomLayer.h"
 
 class LoadingScene;
 class RoomLayer;
+class SettingLayer;
 
-/**
+   /**
    * @brief 主场景
    */
-class MainScene : public Scene
+class MainScene : public cocos2d::Scene
 {
 public:
 
@@ -25,34 +20,51 @@ private:
 
     virtual bool init();
 
-    /**
-    * @brief 设置按钮并add进场景
-    * @param (Vec2 position)位置
-    * @param (std::string pic_name)图片路径
-    */
-    Button* ButtonCreate(Vec2 position, std::string pic_name1, std::string pic_name2 = "");
+    //弃用
+
+    //**
+    //* @brief 设置按钮并add进场景,有两个重载版本
+    //* @param (Vec2 position)位置
+    //* @param (std::string pic_name)图片路径
+    //* @param (std::string words)文字按钮的显示文字
+    //*/
+    //cocos2d::ui::Button* ButtonCreate(const cocos2d::Vec2&& position, const std::string&& pic_name);
+    //cocos2d::ui::Button* ButtonCreate(const std::string&& words,const cocos2d::Vec2&& position);
+
+    MainScene() :kVisibleSize(cocos2d::Director::getInstance()->getVisibleSize())
+        , _bg(NULL), _loading(NULL), _menu_button(NULL), _info_button(NULL), _play_button(NULL)
+        , _figure(NULL), _room(NULL), _set(NULL), _back(NULL) {};
 
 private:
 
-    const Vec2 kVisibleSize = Director::getInstance()->getVisibleSize();
+    const cocos2d::Size kVisibleSize;
 
     //主界面背景
-    Sprite* _bg = NULL;
+    cocos2d::Sprite* _bg;
 
     //loading层
-   LoadingScene* _loading = NULL;
+   LoadingScene* _loading;
 
    //菜单
-   Button* _menu_button = NULL;
+   cocos2d::ui::Button* _menu_button;
 
    //个人信息
-   Button* _info_button = NULL;
+   cocos2d::ui::Button* _info_button;
 
-   Button* _play_button = NULL;
+   //开始游戏按钮
+   cocos2d::ui::Button* _play_button;
 
-   Sprite* _figure = NULL;
+   //场景中显示人物形象
+   cocos2d::Sprite* _figure;
 
    //房间层
-   RoomLayer* _room = NULL;
+   RoomLayer* _room;
+
+   //设置层
+   SettingLayer* _set;
+
+   //返回按钮
+   cocos2d::ui::Button* _back;
+
 };
 
