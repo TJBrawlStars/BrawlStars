2e98c1be6642e3cd38f3c9a90d14723869d3a7f4
Mikeneko-nya <mayaaowu@gmail.com>
2022-05-10 20:10:29 +0800

    feat($Tool)：添加了一个Tool类，包括ui制作封装工具函数、场景切换效果函数
    包含方法：1.重载的用九宫格实现的按钮
                      2.设置Layer吞没点击事件
                      3.设置背景
                      4.enum类存切换场景的效果
                      5.场景切换效果实现
                      6.创建Slider
                      7.创建开关menu
                      8.创建Label

---
+++ Classes/Tool/Tools.cpp
@@ -0,0 +1,185 @@
+#include "Tool/Tools.h"
+USING_NS_CC;
+using namespace ui;
+//#define NDEBUG
+#include<cassert>
+#include "View/LoadingScene.h"
+#include "View/RoomLayer.h"
+
+//这里不能直接给kVisibleSize用director获取，因为全局变量在director获取visiblesize之前就已经做好赋值
+cocos2d::Size Tools::kVisibleSize = Size(0, 0);
+
+cocos2d::ui::Button* Tools::ButtonCreate(const cocos2d::Vec2&& position, const std::string&& pic_name, cocos2d::Node* that)
+{
+	auto button = Button::create("ui/button.png");
+	assert(button != NULL);
+
+	button->setScale9Enabled(true);
+	button->setCapInsets(Rect(5, 5, 15, 15));
+	button->setPosition(position);
+
+	auto sprite = Sprite::create(pic_name);
+	assert(sprite != NULL);
+
+	button->addChild(sprite);
+	button->setContentSize(Size(sprite->getContentSize().width + 50, sprite->getContentSize().height + 25));
+	sprite->setPosition(button->getContentSize() / 2);
+	that->addChild(button);
+	return button;
+}
+
+cocos2d::ui::Button* Tools::ButtonCreate(const std::string&& words, const cocos2d::Vec2&& position, cocos2d::Node* that)
+{
+	auto button = Button::create("ui/button_y.png");
+	assert(button != NULL);
+
+	button->setScale9Enabled(true);
+	button->setCapInsets(Rect(5, 5, 15, 15));
+	button->setPosition(position);
+
+	auto label = Label::createWithSystemFont(words, "微软雅黑", 40);
+	assert(label != NULL);
+	button->setTitleLabel(label);
+	button->setContentSize(Size(label->getContentSize().width + 60, label->getContentSize().height + 25));
+	that->addChild(button);
+	return button;
+}
+
+cocos2d::ui::Button* Tools::ButtonCreate(const cocos2d::Vec2&& position,const std::string&& words, const std::string&& pic_name, const std::string&& pic_name_s, cocos2d::Node* that)
+{
+	auto button = Button::create(pic_name, pic_name_s);
+	assert(button != NULL);
+	button->setPosition(position);
+	button->setScale9Enabled(true);
+	button->setCapInsets(Rect(5, 5, 15, 15));
+
+	auto label = Label::createWithSystemFont(words, "微软雅黑", 40);
+	assert(label != NULL);
+	button->setTitleLabel(label);
+	button->setContentSize(Size(label->getContentSize().width + 120, label->getContentSize().height + 50));
+	that->addChild(button);
+	return button;
+}
+
+void Tools::LayerSwallow(EventListenerTouchOneByOne* listener, cocos2d::Layer* that)
+{
+	listener->setSwallowTouches(true);
+	auto dispatcher = Director::getInstance()->getEventDispatcher();
+	dispatcher->addEventListenerWithSceneGraphPriority(listener, that);
+}
+
+Sprite* Tools::SetBg(const std::string&& filename, cocos2d::Node* that)
+{
+	auto bg = Sprite::create(filename);
+	assert(bg != NULL);
+	that->addChild(bg);
+	bg->setPosition(kVisibleSize / 2);
+	return bg;
+}
+
+cocos2d::ui::Scale9Sprite* Tools::SetBg(cocos2d::Node* that)
+{
+	auto bg = Scale9Sprite::create("ui/button.png");
+	assert(bg != NULL);
+	that->addChild(bg);
+
+	bg->setCapInsets(Rect(5, 5, 15, 15));
+	bg->setPosition(kVisibleSize / 2);
+	bg->setContentSize(kVisibleSize);
+	return bg;
+}
+
+void Tools::SwitchScene(cocos2d::Node* node, SwitchSceneType type)
+{
+	switch (type)
+	{
+	case SwitchSceneType::Down:
+	{
+		auto move1 = MoveTo::create(0.5f, Vec2(0, 0));
+		auto move2 = MoveBy::create(0.15f, Vec2(0, 75));
+		auto move3 = MoveBy::create(0.15f, Vec2(0, -75));
+		node->setPosition(0, kVisibleSize.height);
+		node->runAction(Sequence::create(move1, move2, move3, NULL));
+		break;
+	}
+	case SwitchSceneType::Up:
+	{
+		auto distory = RemoveSelf::create();
+		auto move = MoveTo::create(0.5f, Vec2(0, kVisibleSize.height));
+		auto wait = DelayTime::create(0.2f);
+		node->runAction(Sequence::create(wait, move, distory, NULL));
+		break;
+	}
+	case SwitchSceneType::FadeOut:
+	{
+		//子节点随父节点一起淡入淡出
+		node->setCascadeOpacityEnabled(true);
+		auto delay = DelayTime::create(0.3f);
+		auto fadeout = FadeOut::create(1.f);
+		auto distory = RemoveSelf::create();
+		node->runAction(Sequence::create(delay, fadeout, distory, NULL));
+		break;
+	}
+	case SwitchSceneType::FadeIn:
+	{
+		//子节点随父节点一起淡入淡出
+		node->setCascadeOpacityEnabled(true);
+		//要先设置节点透明才可以有效果
+		node->setOpacity(0);
+		auto delay = DelayTime::create(0.3f);
+		auto fadein = FadeIn::create(1.f);
+		node->runAction(Sequence::createWithTwoActions(delay, fadein));
+		break;
+	}
+	case SwitchSceneType::Turn:
+	{
+		break;
+	}
+	}
+}
+
+cocos2d::ui::Slider* Tools::SliderCreate(cocos2d::Vec2&& pos, std::string&& bar, std::string&& Texture, std::string&& ball, cocos2d::Node* that)
+{
+	auto slider = Slider::create();
+	that->addChild(slider);
+	slider->loadBarTexture(bar);
+	slider->loadProgressBarTexture(Texture);
+	slider->loadSlidBallTextures(ball);
+	slider->setPosition(pos);
+	slider->setPercent(100);
+	return slider;
+}
+
+cocos2d::Menu* Tools::MenuCreate(cocos2d::Vec2&& pos, std::string&& item, std::string&& item_s, const ccMenuCallback& callback , cocos2d::Node* that)
+{
+	auto sound_menuitem = MenuItemImage::create(item, item);
+	auto sound_menuitem_s = MenuItemImage::create(item_s, item_s);
+	auto sound_menutoggle = MenuItemToggle::createWithCallback(callback, sound_menuitem, sound_menuitem_s, NULL);
+	auto soundmenu = Menu::create(sound_menutoggle, NULL);
+	assert(soundmenu != NULL);
+	that->addChild(soundmenu);
+	soundmenu->setPosition(pos);
+	return soundmenu;
+}
+
+cocos2d::Label* Tools::LabelCreateTTF(cocos2d::Vec2&& pos, std::string&& words, std::string&& ttf ,float size, Node* that)
+{
+	auto label = Label::createWithTTF(words, ttf, size);
+	assert(label != NULL);
+	that->addChild(label);
+	label->setPosition(pos);
+	return label;
+}
+
+cocos2d::Label* Tools::LabelCreateSystem(cocos2d::Vec2&& pos, std::string&& words ,std::string&& ttf, float size, Node* that)
+{
+	auto label = Label::createWithSystemFont(words, ttf, size);
+	assert(label != NULL);
+	that->addChild(label);
+	label->setPosition(pos);
+	return label;
+}
+
+
+
+
---
+++ Classes/Tool/Tools.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include"cocos2d.h"
+#include"ui/CocosGUI.h"
+
+/**
+* @brief 工具类，提供各种方法
+*/
+class Tools
+{
+public:
+
+	/**
+	* @brief 初始化一下static成员变量以便后续使用
+	*/
+	static void set() { kVisibleSize = cocos2d::Director::getInstance()->getVisibleSize(); }
+
+	/**
+	* @brief 提供使用九宫格精灵设置图片按钮和文字按钮的封装方法
+	* @param (Vec2 position)位置
+    * @param (std::string pic_name)图片路径
+    * @param (std::string words)文字按钮的显示文字
+    * @param (Scene* that)场景的this指针
+    */
+	static cocos2d::ui::Button* ButtonCreate(const cocos2d::Vec2&& position, const std::string&& pic_name, cocos2d::Node* that);
+	static cocos2d::ui::Button* ButtonCreate(const std::string&& words, const cocos2d::Vec2&& position, cocos2d::Node* that);
+	static cocos2d::ui::Button* ButtonCreate(const cocos2d::Vec2&& position, const std::string&& words, const std::string&& pic_name
+		                                   , const std::string&& pic_name_s, cocos2d::Node* that);
+
+	/**
+	* @brief 设置layer吞没点击事件
+	* @param (Scene* that)场景的this指针
+	* @param (EventListenerTouchOneByOne* listener）点击listener
+	*/
+	static void LayerSwallow(cocos2d::EventListenerTouchOneByOne* listener, cocos2d::Layer* that);
+
+	/**
+	* @brief 设置背景
+	* @param (Scene* that)场景的this指针
+	* @param (const std::string&&filename)图片地址
+	*/
+	static cocos2d::Sprite* SetBg(const std::string&& filename, cocos2d::Node* that);
+	static cocos2d::ui::Scale9Sprite* SetBg(cocos2d::Node* that);
+
+	/**
+	* @brief 切换场景的action的一些类型
+	*/
+	enum class SwitchSceneType 
+	{
+		Down,
+		Up,
+		FadeIn,
+		FadeOut,
+		Turn
+	};
+
+	/**
+	* @brief 切换场景
+	* @param (cocos2d::Node*)添加动作的节点
+	* @param (SwitchSceneType)类型
+	*/
+	static void SwitchScene(cocos2d::Node*, SwitchSceneType);
+
+	/**
+	* @brief 封装创建slider
+	* @param (cocos2d::Node*)要添加进的节点
+	*/
+	static cocos2d::ui::Slider* SliderCreate(cocos2d::Vec2&& pos, std::string&& bar, std::string&& Texture, std::string&& ball, cocos2d::Node*);
+
+	/**
+	* @brief 封装创建开关Menu
+	* @param (cocos2d::Node*)要添加进的节点
+	* @param (cocos2d::ccMenuCallback& callback)回调函数
+	*/
+	static cocos2d::Menu* MenuCreate(cocos2d::Vec2&& pos,std::string&& item, std::string&& item_s
+		, const cocos2d::ccMenuCallback& callback , cocos2d::Node* that);
+
+	/**
+	* @brief 封装创建Label
+	* @param (cocos2d::Node*)要添加进的节点
+	* @param (cocos2d::ccMenuCallback& callback)回调函数
+	*/
+	static cocos2d::Label* LabelCreateTTF(cocos2d::Vec2&& pos, std::string&& words, std::string&& ttf, float size, cocos2d::Node*);
+	static cocos2d::Label* LabelCreateSystem(cocos2d::Vec2&& pos, std::string&& words, std::string&& ttf, float size, cocos2d::Node*);
+
+private:
+
+	//这里不能直接给kVisibleSize用director获取，因为全局变量在director获取visiblesize之前就已经做好赋值
+	static cocos2d::Size kVisibleSize;
+
+	Tools();
+
+};
+
