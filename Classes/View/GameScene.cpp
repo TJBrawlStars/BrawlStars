e2e47bf9ef32339c387d950db86a986d3c8dd2ff
MercedesKK <2455650395@qq.com>
2022-05-16 00:46:07 +0800

    chore:merge new GameScene

--- Classes/View/GameScene.cpp
+++ Classes/View/GameScene.cpp
@@ -5,8 +5,18 @@
 
 bool GameScene::init()
 {
-	if (!Scene::initWithPhysics()) 
+	if (!Scene::init() || !Scene::initWithPhysics())
 		return false;
+	//设置场景的tag，后续可能会用
+	this->setTag(SceneTag::kGameSceneTag);
+
+	//开始计时
+	auto timer = MyTimer::create();
+	timer->createUI();
+	this->addChild(timer, kTimerPriority);
+
+	//物理检测红框
+	this->getPhysicsWorld()->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
 
 	//这是王佳垚写的，我先停掉了，停在我这个地图界面
 
@@ -24,22 +34,29 @@ bool GameScene::init()
 			SceneManager::getInstance()->changeScene(SceneManager::EnumSceneType::en_SettlementScene);
 		});*/
 
-	//初始化地图
-	initMap();
-	//初始化障碍层
-	createBarrier();
-	//初始化宝箱
-	addRandomBox();
 
-	// Add BeiyaHero
-	_player = Beiya::createBeiya();
-	_player->setPosition(Vec2(100, 100));
-	this->addChild(_player);
+
+
+
+		//初始化地图
+	addMap();
+	//初始化障碍层
+	addBarrier();
+	//初始化草丛
+	addGrass();
+	//添加主角
+	addHeroPlayer();
+
+
+
+
 	this->scheduleUpdate();
 	
 
-	poisonCircle* _layer= poisonCircle::create();
-	this->addChild(_layer);
+	//poisonCircle* _layer= poisonCircle::create();
+	//this->addChild(_layer);
+
+
 	return true;
 }
 
@@ -52,87 +69,92 @@ void GameScene::update(float dt)
 	//use the keyboard to move hero
 	_player->setKeyboardListener(true);
 	setViewPointByPlayer(_player->getPosition());
-}
-bool poisonCircle::init()
-{
-	if (!Scene::init())
-		return false;
-	
-	//定时器，初步定游戏开始之后...才开始蔓延,大概计算需要（15次）多长时间就算是蔓延到覆盖整个地图了，每3秒更新一次
-	schedule(SEL_SCHEDULE(&poisonCircle::updatepoisonCircle), 3.0f, 15, 3.0f);
 
-	return true;
-}
-//加标签的问题
-void poisonCircle::updatepoisonCircle(float dt)
-{
-
-	//毒圈起始锚点位置
-	poisonCircleDown = cocos2d::Sprite::create("circledown.png");
-	poisonCircleDown->setPosition(1200, 0);
-	addChild(poisonCircleDown);
-	poisonCircleUp = cocos2d::Sprite::create("circleup.png");
-	poisonCircleUp->setPosition(1200,2400);
-	addChild(poisonCircleUp);
-	poisonCircleLeft = cocos2d::Sprite::create("circleleft.png");
-	poisonCircleLeft->setPosition(0,1200);
-	addChild(poisonCircleLeft);
-	poisonCircleRight = cocos2d::Sprite::create("circleright.png");
-	poisonCircleRight->setPosition(2400,1200);
-	addChild(poisonCircleRight);
-
-	
-	if (poisonCircleDown->getPosition().y < poisonCircleMax)
-	{
-		poisonCircleDown->setPosition(poisonCircleDown->getPosition().x, poisonCircleDown->getPosition().y + poisonCircleMarch);
-	}
-
-	if (poisonCircleUp->getPosition().y < poisonCircleMax)
-	{
-		poisonCircleUp->setPosition(poisonCircleUp->getPosition().x, poisonCircleUp->getPosition().y + poisonCircleMarch);
-	}
-	if (poisonCircleLeft->getPosition().y < poisonCircleMax)
-	{
-		poisonCircleLeft->setPosition(poisonCircleLeft->getPosition().x, poisonCircleLeft->getPosition().y + poisonCircleMarch);
-	}
-	if (poisonCircleRight->getPosition().y < poisonCircleMax)
-	{
-		poisonCircleRight->setPosition(poisonCircleRight->getPosition().x, poisonCircleRight->getPosition().y + poisonCircleMarch);
-	}
-
-	//物理引擎
-	float downHeight = poisonCircleDown->getPosition().y;
-	//设置刚体
-	auto downpoisonBody = PhysicsBody::createBox(Size(2400, downHeight));
-	poisonCircleDown->addComponent(downpoisonBody);
-	poisonCircleDown->getPhysicsBody()->setDynamic(false);
-	poisonCircleDown->getPhysicsBody()->setCategoryBitmask(0x10);
-	poisonCircleDown->getPhysicsBody()->setContactTestBitmask(0x10);
-	poisonCircleDown->getPhysicsBody()->setGravityEnable(false);
-	
-
-	float upHeight = poisonCircleUp->getPosition().y;
-	auto uppoisonBody = PhysicsBody::createBox(Size(2400, upHeight));
-	poisonCircleUp->addComponent(uppoisonBody);
-	poisonCircleUp->getPhysicsBody()->setDynamic(false); //静态刚体
-	poisonCircleUp->getPhysicsBody()->setCategoryBitmask(0x10);
-	poisonCircleUp->getPhysicsBody()->setContactTestBitmask(0x10);
-	
-	float leftHeight = poisonCircleLeft->getPosition().y;
-	auto leftpoisonBody = PhysicsBody::createBox(Size(2400, leftHeight));
-	poisonCircleLeft->addComponent(leftpoisonBody);
-	poisonCircleLeft->getPhysicsBody()->setDynamic(false);
-	poisonCircleLeft->getPhysicsBody()->setCategoryBitmask(0x10);
-	poisonCircleLeft->getPhysicsBody()->setContactTestBitmask(0x10);
-	
-
-	float rightHeight = poisonCircleRight->getPosition().y;
-	auto rightpoisonBody = PhysicsBody::createBox(Size(2400, rightHeight));
-	poisonCircleRight->addComponent(rightpoisonBody);
-	poisonCircleRight->getPhysicsBody()->setDynamic(false);
-	poisonCircleRight->getPhysicsBody()->setCategoryBitmask(0x10);
-	poisonCircleRight->getPhysicsBody()->setContactTestBitmask(0x10);
-
+	//进入草丛后边透明
+	grassPlayerOpacity();
 }
 
-
+//
+//bool poisonCircle::init()
+//{
+//	if (!Scene::init())
+//		return false;
+//	
+//	//定时器，初步定游戏开始之后...才开始蔓延,大概计算需要（15次）多长时间就算是蔓延到覆盖整个地图了，每3秒更新一次
+//	schedule(SEL_SCHEDULE(&poisonCircle::updatepoisonCircle), 3.0f, 15, 3.0f);
+//
+//	return true;
+//}
+////加标签的问题
+//void poisonCircle::updatepoisonCircle(float dt)
+//{
+//
+//	//毒圈起始锚点位置
+//	poisonCircleDown = cocos2d::Sprite::create("circledown.png");
+//	poisonCircleDown->setPosition(1200, 0);
+//	addChild(poisonCircleDown);
+//	poisonCircleUp = cocos2d::Sprite::create("circleup.png");
+//	poisonCircleUp->setPosition(1200,2400);
+//	addChild(poisonCircleUp);
+//	poisonCircleLeft = cocos2d::Sprite::create("circleleft.png");
+//	poisonCircleLeft->setPosition(0,1200);
+//	addChild(poisonCircleLeft);
+//	poisonCircleRight = cocos2d::Sprite::create("circleright.png");
+//	poisonCircleRight->setPosition(2400,1200);
+//	addChild(poisonCircleRight);
+//
+//	
+//	if (poisonCircleDown->getPosition().y < poisonCircleMax)
+//	{
+//		poisonCircleDown->setPosition(poisonCircleDown->getPosition().x, poisonCircleDown->getPosition().y + poisonCircleMarch);
+//	}
+//
+//	if (poisonCircleUp->getPosition().y < poisonCircleMax)
+//	{
+//		poisonCircleUp->setPosition(poisonCircleUp->getPosition().x, poisonCircleUp->getPosition().y + poisonCircleMarch);
+//	}
+//	if (poisonCircleLeft->getPosition().y < poisonCircleMax)
+//	{
+//		poisonCircleLeft->setPosition(poisonCircleLeft->getPosition().x, poisonCircleLeft->getPosition().y + poisonCircleMarch);
+//	}
+//	if (poisonCircleRight->getPosition().y < poisonCircleMax)
+//	{
+//		poisonCircleRight->setPosition(poisonCircleRight->getPosition().x, poisonCircleRight->getPosition().y + poisonCircleMarch);
+//	}
+//
+//	//物理引擎
+//	float downHeight = poisonCircleDown->getPosition().y;
+//	//设置刚体
+//	auto downpoisonBody = PhysicsBody::createBox(Size(2400, downHeight));
+//	poisonCircleDown->addComponent(downpoisonBody);
+//	poisonCircleDown->getPhysicsBody()->setDynamic(false);
+//	poisonCircleDown->getPhysicsBody()->setCategoryBitmask(0x10);
+//	poisonCircleDown->getPhysicsBody()->setContactTestBitmask(0x10);
+//	poisonCircleDown->getPhysicsBody()->setGravityEnable(false);
+//	
+//
+//	float upHeight = poisonCircleUp->getPosition().y;
+//	auto uppoisonBody = PhysicsBody::createBox(Size(2400, upHeight));
+//	poisonCircleUp->addComponent(uppoisonBody);
+//	poisonCircleUp->getPhysicsBody()->setDynamic(false); //静态刚体
+//	poisonCircleUp->getPhysicsBody()->setCategoryBitmask(0x10);
+//	poisonCircleUp->getPhysicsBody()->setContactTestBitmask(0x10);
+//	
+//	float leftHeight = poisonCircleLeft->getPosition().y;
+//	auto leftpoisonBody = PhysicsBody::createBox(Size(2400, leftHeight));
+//	poisonCircleLeft->addComponent(leftpoisonBody);
+//	poisonCircleLeft->getPhysicsBody()->setDynamic(false);
+//	poisonCircleLeft->getPhysicsBody()->setCategoryBitmask(0x10);
+//	poisonCircleLeft->getPhysicsBody()->setContactTestBitmask(0x10);
+//	
+//
+//	float rightHeight = poisonCircleRight->getPosition().y;
+//	auto rightpoisonBody = PhysicsBody::createBox(Size(2400, rightHeight));
+//	poisonCircleRight->addComponent(rightpoisonBody);
+//	poisonCircleRight->getPhysicsBody()->setDynamic(false);
+//	poisonCircleRight->getPhysicsBody()->setCategoryBitmask(0x10);
+//	poisonCircleRight->getPhysicsBody()->setContactTestBitmask(0x10);
+//
+//}
+//
+//
--- Classes/View/GameScene.h
+++ Classes/View/GameScene.h
@@ -17,24 +17,52 @@ public:
     virtual bool init();
 
     /********* @author:张靖凯************/
-    //传入主角的坐标，放在控制器（控制角色）当中
+
+       //传入主角的坐标，放在控制器（控制角色）当中
     void setViewPointByPlayer(cocos2d::Point position);
 
+    Beiya* getBeiya() { return _player; };
 
-    //判断是否碰撞障碍物（有bug！）(暂时不用)
-    //传入的是openGL坐标(就是左下角为原点，2400*2400）向右为x向上为y
-    //bool isColliding(Vec2 position);
 
     //转换成瓦片坐标（左上角为原点）
     cocos2d::Vec2 tiledCoordFromPosition(cocos2d::Vec2 position);
 
+
+    //返回障碍层/草丛
+    cocos2d::Vector<cocos2d::Sprite*> getObstacle();
+    cocos2d::Vector<cocos2d::Sprite*> getGrass();
+
+
+    //判断是否有障碍物/草丛,有则返回true
+    bool isBarrierExist(cocos2d::Point position);
+    bool isGrassExist(cocos2d::Point position);
+
+    void grassPlayerOpacity();
+
     //初始化地图
-    void initMap();
+    void addMap();
+
+
+    //初始化角色
+    void addHeroPlayer();
+
+
+    //void addAIHeroPlayer(); 
+
 
     //创建障碍层
-    void createBarrier();
+    void addBarrier();
 
 
+    //创建草丛
+    void addGrass();
+
+    void inTimerViewScreen();
+
+    //屏幕中心的位置
+    Vec2 destPos(Point position);
+
+    /********* @author:王琳************/
     //schedule update
     void update(float dt);
     
@@ -44,42 +72,43 @@ private:
     const Vec2 kVisibleSize = Director::getInstance()->getVisibleSize();
 
 
-
-
     /********* @author:张靖凯************/
 
     //地图信息（initMap()函数初始化了里面的变量）
     MapInfo _mapinfo;
+    //障碍容器
+    cocos2d::Vector<cocos2d::Sprite*> _obstacle;
+    //草丛容器
+    cocos2d::Vector<cocos2d::Sprite*> _grass;
     //主角
     Beiya* _player;
    
 };
 
-/********* @author:王琳************/
-class poisonCircle :public Scene
-{
-public:
-
-    //static Scene* scene();
-    CREATE_FUNC(poisonCircle);
-
-
-    ////添加宝箱
-    //void addRandomBox();
-    //void addBox();
-
-
-    /********* @author:王琳************/
-    
-    void update(float dt);
-
-    float poisonCircleMarch = 0.7f;
-    float poisonCircleMax = 2400;
-
-   // int poisonCircleDamage = 10;
-   // float poisonCircleInterval = 3;
-
-   // bool poisonCircleCanDamage = true;
-
-};
-
+///********* @author:王琳************/
+//class poisonCircle :public Scene
+//{
+//public:
+//
+//    //static Scene* scene();
+//    CREATE_FUNC(poisonCircle);
+//
+//
+//
+//    virtual bool init();
+//
+//    void updatepoisonCircle(float dt);
+//
+//    cocos2d::Sprite* poisonCircleDown;
+//    cocos2d::Sprite* poisonCircleUp;
+//    cocos2d::Sprite* poisonCircleLeft;
+//    cocos2d::Sprite* poisonCircleRight;
+//    float poisonCircleMarch = 0.7f;
+//    float poisonCircleMax = 2400;
+//
+//    // int poisonCircleDamage = 10;
+//    // float poisonCircleInterval = 3;
+//
+//    // bool poisonCircleCanDamage = true;
+//
+//};
