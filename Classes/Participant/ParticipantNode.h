0ff49fad44112cbe5edf9e68faecb7ffe6092611
sty <2969627331@qq.com>
2022-05-29 00:15:14 +0800

    feat(participant):add username

--- Classes/Participant/ParticipantNode.h
+++ Classes/Participant/ParticipantNode.h
@@ -18,14 +18,16 @@ public:
 	virtual void setPosition(float x, float y)              override { return _hero->setPosition(x, y); }
 	virtual const cocos2d::Vec2& getPosition() const        override { return _hero->getPosition(); }
 
-	void setHeroType(std::string HeroID);
+	void setHeroType(std::string heroID);
+	void setUserName(std::string userName) { _userName = userName; }
 
 	bool alive()              const noexcept { return _hero->alive(); }
+	std::string getHeroType() const noexcept { return _heroID; }
 	std::string getUserName() const noexcept { return _userName; }
 
 protected:
 	std::string _userName = "???";  ///< the participant's name
-	std::string _heroid;            ///< use hero id to create hero
+	std::string _heroID;            ///< use hero id to create hero
 	Hero* _hero;                    ///< the hero the participant use
 
 	/** instantiation is not allowed */
