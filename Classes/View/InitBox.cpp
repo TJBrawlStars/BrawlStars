6eb80c3e4738f4b1157c61db295e82b1027c6da2
LinWang1225 <534605478@qq.com>
2022-05-29 00:13:36 +0800

    feat(gamescene):add treasurebox and fix poisoncircle

---
+++ Classes/View/InitBox.cpp
@@ -0,0 +1,16 @@
+//2152590 王琳
+
+#include"GameScene.h"
+#include"Treasure/TreasureBoxSprite.h"
+
+
+void GameScene::addBox()
+{
+	//宝箱随机出现在地图上，不包括障碍物的位置
+	for (int i = 0; i < 10; ++i)
+	{
+		auto _box = TreasureBox::createTreasureBox();
+		_box->setPosition(randomPosWithoutBarrier());
+		this->addChild(_box);
+	}
+}
