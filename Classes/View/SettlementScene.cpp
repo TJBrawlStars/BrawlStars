0a4563fa685979ac373b5ff46af1b6ee975eb230
Mikeneko-nya <mayaaowu@gmail.com>
2022-06-10 23:20:12 +0800

    fix：解决了结算场景显示bug

--- Classes/View/SettlementScene.cpp
+++ Classes/View/SettlementScene.cpp
@@ -6,11 +6,11 @@ using namespace ui;
 #include "Tool/SceneManager.h"
 #include"Tool/Tools.h"
 #include"Tool/Data.h"
-#include "Const/Const.h"
-#include"Tool/LeaderBoard.hpp"
+//#include "Const/Const.h"
+//#include"Tool/LeaderBoard.hpp"
 #pragma execution_character_set("utf-8")  
 
-extern LeaderBoard<std::string, Rank> single;
+//extern LeaderBoard<std::string, Rank> single;
 
 bool SettlementScene::init()
 {
@@ -38,26 +38,26 @@ bool SettlementScene::init()
 
 void SettlementScene::Win()
 {
-	//读游戏控制器的操作，这边就先直接win叭
-	unsigned int ranking = single.rank(PlistData::getDataByType(PlistData::DataType::ID));
-	assert(ranking <= 10 && ranking >= 1);
-	unsigned int cups = single.find(PlistData::getDataByType(PlistData::DataType::ID))->second->second._trophy;
+	////读游戏控制器的操作，这边就先直接win叭
+	//unsigned int ranking = single.rank(PlistData::getDataByType(PlistData::DataType::ID));
+	//assert(ranking <= 10 && ranking >= 1);
+	//unsigned int cups = single.find(PlistData::getDataByType(PlistData::DataType::ID))->second->second._trophy;
 
-	//获得奖杯数目
-	auto cupslabel = Tools::LabelCreateSystem(Vec2(kVisibleSize.width / 2, kVisibleSize.height * 0.9f), "获得奖杯", "微软雅黑", 35, this);
-	cupslabel->setAnchorPoint(Vec2::ANCHOR_MIDDLE_RIGHT);
-	_cups= Tools::LabelCreateSystem(Vec2(kVisibleSize.width / 2, kVisibleSize.height * 0.9f), "", "微软雅黑", 35, this);
-	_cups->setAnchorPoint(Vec2::ANCHOR_MIDDLE_LEFT);
+	////获得奖杯数目
+	//auto cupslabel = Tools::LabelCreateSystem(Vec2(kVisibleSize.width / 2, kVisibleSize.height * 0.9f), "获得奖杯", "微软雅黑", 35, this);
+	//cupslabel->setAnchorPoint(Vec2::ANCHOR_MIDDLE_RIGHT);
+	//_cups= Tools::LabelCreateSystem(Vec2(kVisibleSize.width / 2, kVisibleSize.height * 0.9f), "", "微软雅黑", 35, this);
+	//_cups->setAnchorPoint(Vec2::ANCHOR_MIDDLE_LEFT);
 
-	//获得金币数量(根据模式不同改变，但只有一个模式就定100叭)
-	auto moneylabel = Tools::LabelCreateSystem(Vec2(kVisibleSize.width / 2, kVisibleSize.height * 0.8f), "获得金币", "微软雅黑", 35, this);
-	moneylabel->setAnchorPoint(Vec2::ANCHOR_MIDDLE_RIGHT);
-	_money = Tools::LabelCreateSystem(Vec2(kVisibleSize.width / 2, kVisibleSize.height * 0.8f), "   100", "微软雅黑", 35, this);
-	_money->setAnchorPoint(Vec2::ANCHOR_MIDDLE_LEFT);
-	PlistData::WriteDataByType(PlistData::DataType::Money, Value(Value(PlistData::getDataByType(PlistData::DataType::Money)).asInt() + 100).asString());
+	////获得金币数量(根据模式不同改变，但只有一个模式就定100叭)
+	//auto moneylabel = Tools::LabelCreateSystem(Vec2(kVisibleSize.width / 2, kVisibleSize.height * 0.8f), "获得金币", "微软雅黑", 35, this);
+	//moneylabel->setAnchorPoint(Vec2::ANCHOR_MIDDLE_RIGHT);
+	//_money = Tools::LabelCreateSystem(Vec2(kVisibleSize.width / 2, kVisibleSize.height * 0.8f), "   100", "微软雅黑", 35, this);
+	//_money->setAnchorPoint(Vec2::ANCHOR_MIDDLE_LEFT);
+	//PlistData::WriteDataByType(PlistData::DataType::Money, Value(Value(PlistData::getDataByType(PlistData::DataType::Money)).asInt() + 100).asString());
 
-	auto label = Tools::LabelCreateTTF(Vec2(kVisibleSize.width / 2 - 450, kVisibleSize.height - 100), "第 " + Value(ranking).asString() + " 名", "fonts/arial.ttf", 75, this);
+	//auto label = Tools::LabelCreateTTF(Vec2(kVisibleSize.width / 2 - 450, kVisibleSize.height - 100), "第 " + Value(ranking).asString() + " 名", "微软雅黑", 75, this);
 
-	_cups->setString("   " + Value(cups).asString());
-	PlistData::WriteDataByType(PlistData::DataType::Cups, Value(Value(PlistData::getDataByType(PlistData::DataType::Cups)).asInt() + cups).asString());
+	//_cups->setString("   " + Value(cups).asString());
+	//PlistData::WriteDataByType(PlistData::DataType::Cups, Value(Value(PlistData::getDataByType(PlistData::DataType::Cups)).asInt() + cups).asString());
 }
