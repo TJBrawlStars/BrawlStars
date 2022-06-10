bf710c1869981841dcee9e025cf7dda423f5ff90
Mikeneko-nya <mayaaowu@gmail.com>
2022-06-10 23:14:48 +0800

    feat($Net)：初始化网络库的操作，为创建socket做准备

---
+++ Classes/Net/Socket.cpp
@@ -0,0 +1,22 @@
+#include "Socket.h"
+
+bool Socket::init_Socket()
+{
+	WSAData wsadata;
+	if (0 != WSAStartup(MAKEWORD(2, 2), &wsadata))//windows socket ansyc(windows异步套接字),这第一个参数WORD类型好像就是指定版本的
+	{
+		err("WSAStartup");
+		return false;
+	}
+	return true;
+}
+
+bool Socket::close_Socket()
+{
+	if (0 != WSACleanup())
+	{
+		err("WSACleanup");
+		return false;
+	}
+	return true;
+}
---
+++ Classes/Net/Socket.h
@@ -0,0 +1,28 @@
+#pragma once
+#include<WinSock2.h>
+#include <WS2tcpip.h>
+#include"cocos2d.h"
+#pragma comment(lib,"ws2_32.lib")
+
+//宏定义一个err，显示问题
+#define err(errMsg) CCLOG(cocos2d::Value(__LINE__).asString().c_str());
+
+//使用端口
+constexpr int kPORT = 7777;
+//缓冲区大小
+constexpr int kMAX = 100;
+
+/**
+* @brief 用于初始化和关闭网络库
+*/
+class Socket
+{
+public:
+	//初始化网络库
+	static bool init_Socket();
+
+	//关闭网络库
+	static bool close_Socket();
+
+};
+
